0ddf3f0056bb33d2d51d9bf5f9a96f04dfd9e7db
VvanGemert <vincent@floorplanner.com>
2015-12-21 15:13:24 +0100

    Added test case for reversed hash resulting in array values

--- test/europe/countries/country_test.rb
+++ test/europe/countries/country_test.rb
@@ -11,6 +11,10 @@ module Europe
           Europe::Countries::Reversed.generate(:name)
         assert_equal reversed_hash['Netherlands'], :NL
         assert_equal reversed_hash['Ireland'], :IE
+
+        reversed_hash =
+          Europe::Countries::Reversed.generate(:currency)
+        assert_equal reversed_hash[:EUR].count, 20
       end
     end
   end
